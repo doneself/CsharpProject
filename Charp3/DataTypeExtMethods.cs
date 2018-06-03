e0ef4b349e037413c7760d5e8e2aeae8dd589879
doneself <doneself@qq.com>
2018-06-03 16:54:50 +0800

    commit

---
+++ Charp3/DataTypeExtMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charp3
+{
+    public static class DataTypeExtMethods
+    {
+        public static string Base64EncodeBytes(this byte[] inputBytes) => (Convert.ToBase64String(inputBytes));
+    }
+}
